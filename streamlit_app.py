b1f85946e6a3286222b06f03c6af7c41e76284a4
tnguyen124893 <nguyensontung5794@gmail.com>
2023-01-27 17:04:31 +0700

    refactor: function-lize code that displays input fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display the table of fruit list on page
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #New section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,10 +39,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.write('The user entered ', fruit_choice)
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
