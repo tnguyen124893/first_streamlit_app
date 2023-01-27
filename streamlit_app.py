8a2cf05da35b2120cb0e38979938d6b6acfce73f
tnguyen124893 <nguyensontung5794@gmail.com>
2023-01-27 16:56:09 +0700

    move the fruityvice code into a Try-Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,18 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.write('The user entered ', fruit_choice)
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#Take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#Output it to a screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
